2d3a70f32b83bbabe1d228cc62b08b4a84d85272
matthewFisher <m.r.f.48215@gmail.com>
2019-05-28 00:03:01 +0900

    initial setup to work with rails server

---
+++ src/components/Landing.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul v-for="user in allPosts">
+      <li>内容：　{{user.email}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import gql from 'graphql-tag'
+
+  const POSTS_QUERY = gql`
+query allPosts{
+  allUsers {
+    title
+    content
+  }
+}`;
+
+  export default {
+    name: 'index',
+    data () {
+      return {
+        allPosts: []
+      }
+    },
+    apollo: {
+      allPosts: {
+        query:POSTS_QUERY
+      }
+    }
+  }
+
+</script>
